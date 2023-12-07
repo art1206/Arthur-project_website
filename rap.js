35c1f1fc7a64d70f0fe3509480cf7cb50420901e
art1206 <144734207+art1206@users.noreply.github.com>
2023-12-07 14:14:16 -0700

    Update rap.js

--- rap.js
+++ rap.js
@@ -1,2 +1,6 @@
 let person={age:44, name:"Jordan" ,favoriteColor:"Orange"};
 window.alert("Age is "+ person.age + "\nName is " + person.name + " Favorite Color is " + person.favoriteColor);
+function birthday(){
+  person.age=person.age + 1;
+  window.alert("Age is "+ person.age + "\nName is " + person.name + " Favorite Color is " + person.favoriteColor);
+}
